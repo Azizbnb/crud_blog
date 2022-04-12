e444adc3d47a7d8f4b3d79ad60408ea569a9103c
GregSimplelab <gz@simplelab.info>
2022-04-12 17:37:15 +0200

    Roles

--- database/migrations/2022_04_12_131121_create_roles_table.php
+++ database/migrations/2022_04_12_131121_create_roles_table.php
@@ -13,14 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('role_id')->default(3);
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('libelle');
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
